ed91c9cd01f53dd6c74f1a993c35cf96e1000f88
Abdurrahman Mukhtar <99673732+abdullCoder@users.noreply.github.com>
2022-11-19 04:05:54 -0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const port = 3000;
+const app = express()
+const url = 'mongodb://localhost:27017/firstPL'
+const cors = require('cors')
+require('dotenv').config
 
+mongoose.connect(url)
+.then(success => {
+    console.log('db connect succesfull');
+})
+.catch(err => {
+    console.log('db not connect, error ' + err);
+})
+
+app.use(express.json())
+const router = require('./router/router')
+app.use(router)
+
+app.get('/', (req,res)=>{
+    res.send('Hello this is zuri Assignment')
+})
+
+app.listen(port, ()=>{
+    console.log('app listening on port ' + port);
+})
